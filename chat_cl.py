10efebef8509e11716c190d721e62df648f755f6
apinyacode <apinya@gmail.com>
2020-08-03 14:35:37 +0100

    update intents and adding command line version

--- chat_cl.py
+++ chat_cl.py
@@ -11,7 +11,7 @@ import random
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 labels = pickle.load(open('labels.pkl','rb'))
-
+#context = {}
 
 def clean_up_sentence(sentence):
     # tokenize the pattern - split words into array
@@ -63,61 +63,18 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-"""
-#Creating GUI with tkinter
-import tkinter
-from tkinter import *
-"""
+
 kill_groot = False
-#def send():
-    # msg = EntryBox.get("1.0",'end-1c').strip()
-    # EntryBox.delete("0.0",END)
+
 while kill_groot != True:
     msg = input('You: ')
 
     if msg != '':
-    #ChatLog.config(state=NORMAL)
-    #ChatLog.insert(END, "You: " + msg + '\n\n')
-    #print("You: " + msg + '\n')
-    #ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
     
-    res = chatbot_response(msg)
-    #ChatLog.insert(END, "Groot: " + res + '\n\n')
-    print("Groot: " + res + '\n')    
-    #ChatLog.config(state=DISABLED)
-    #ChatLog.yview(END)
-    if msg == 'Bye':
+        res = chatbot_response(msg)
+        print("Groot: " + res + '\n')    
+    
+    if msg.lower() == 'bye':
         kill_groot = True
  
-"""
-base = Tk()
-base.title("I am Groot")
-base.geometry("400x500")
-base.resizable(width=TRUE, height=TRUE)
-"""
-#Create Chat window
-#ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
 
-#ChatLog.config(state=DISABLED)
-
-#Bind scrollbar to Chat window
-#scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-#ChatLog['yscrollcommand'] = scrollbar.set
-
-#Create Button to send message
-#SendButton = Button(base, font=("Verdana",9,'bold'), text="Send", width="12", height=5,
-     #               bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
-      #              command= send )
-
-#Create the box to enter message
-#EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
-
-
-#Place all components on the screen
-#scrollbar.place(x=376,y=6, height=386)
-#ChatLog.place(x=6,y=6, height=386, width=370)
-#EntryBox.place(x=128, y=401, height=90, width=265)
-#SendButton.place(x=6, y=401, height=90)
-
-#base.mainloop()
